0523035024287901061d5cd3d58c7c6dede17099
srinath ragalla <srinathragalla17@gmail.com>
2021-12-14 23:37:39 +0530

    assignment operators

---
+++ assignment oprs.js
@@ -0,0 +1,43 @@
+var num1=50;
+var num2=40;
+var num3=30;
+
+
+num1 += 25;
+console.log(num1);
+
+
+num2 -=20 ;
+console.log(num2);
+
+num3 *= 3;
+console.log(num3);
+
+
+ 
+num1 /= 5;
+console.log(num1);
+
+
+
+num2 %= 2;
+console.log(num2);
+
+
+var num=10;
+num <<=10;
+console.log(num);
+
+
+var num4=15;
+num4 >>=9;
+console.log(num4);
+
+var num5=5;
+num5 &=5;
+console.log(num5);
+
+
+var num6=20;
+num6 ^=5;
+console.log(num6);
